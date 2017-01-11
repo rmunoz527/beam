3a574183aa47c5db41a2726cc17464c0368bdd99
Robert Munoz <rmunoz527@gmail.com>
2017-01-11 11:47:37 -0800

    [BEAM-216] Added ParquetIO

---
+++ sdks/java/io/parquet/src/main/java/org/apache/beam/sdk/io/parquet/ParqeutIO.java
@@ -0,0 +1,7 @@
+package org.apache.beam.sdk.io.parquet;
+
+/**
+ * Created by rmunoz on 1/11/17.
+ */
+public class ParqeutIO {
+}
---
+++ sdks/java/io/parquet/src/main/java/org/apache/beam/sdk/io/parquet/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by rmunoz on 1/11/17.
+ */
+package org.apache.beam.sdk.io.parquet;
